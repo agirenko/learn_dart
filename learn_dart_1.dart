3104fcc22e90c20c1087dc57059cd82dd0facf3d
agirenko <aspnet123@gmail.com>
2018-07-14 15:04:42 +0300

    third commit

--- learn_dart_1.dart
+++ learn_dart_1.dart
@@ -1,3 +1,5 @@
+//https://www.dartlang.org/guides/language/language-tour
+
 variables() {
 //  Variables store references. The variable called name contains a reference to a String object with a value of “Bob”.
 //  The type of the name variable is inferred to be String, but you can change that type by specifying it.
@@ -135,10 +137,15 @@ lists() {
 
   var constantList = const [1, 2, 3];
 // constantList[1] = 1; // Uncommenting this causes an error.
+  print(constantList);
+}
+//TODO
+maps(){
 
 }
 
 main() {
   variables();
   lists();
+  maps();
 }
