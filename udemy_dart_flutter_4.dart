d8c36b3e40a7aa61c0f235f129a46af167c9375d
agirenko <aspnet123@gmail.com>
2018-07-15 22:42:22 +0300

    Future example is added (Udemy, Stephen Grider, Flutter course) and final keyword

--- udemy_dart_flutter_4.dart
+++ udemy_dart_flutter_4.dart
@@ -1,13 +1,23 @@
 import 'dart:async';
 // work with Future example
 
-main() {
-  var result = get('http://some.url');
+main() async {
+// JavaScript Promise like code
+//  print('About to fetch data...');
+//  get('http://some.url').then((result) {
+//    print('Get result: ' + result);
+//  });
+//  print('Get is started but result is not ready yet...');
+
+// async-await code (recommended)
+  print('About to fetch data...');
+  var result = await get('http://some.url');
+  print('Awaiting result...'); // this string is not shown while awaiting the result
   print(result);
 }
 
 Future<String> get(String url) {
-  return new Future.delayed(new Duration(seconds: 3), () {
+  return new Future.delayed(new Duration(seconds: 2), () {
     return 'Got the data!';
   });
 }
---
+++ udemy_dart_flutter_5.dart
@@ -0,0 +1,7 @@
+void main() {
+  // final keyword
+  final String myName = 'Stephen';
+  var myNumber = 1243;
+  //myName = "abc"; // prohibited
+  print(myName);
+}
