73328f44cec07a68fd6de4cdea9e65b1f9a36bc3
agirenko <aspnet123@gmail.com>
2018-07-20 12:52:27 +0300

    Started "Flutter & Dart" Paulo Dichone

--- flutter_dart_udemy_paulo/declaring_variables.dart
+++ flutter_dart_udemy_paulo/declaring_variables.dart
@@ -1,9 +1,18 @@
 printHello(String who) {
-  print('Hello ' + who);
+  print('Hello ' + who + '!');
 }
 
 main(List<String> arguments) {
   var country = 'Mozambique'; // string
+  String name;
+  name = "Some Name";
+
+  dynamic someVar;
+  someVar = 'CountryString';
+  someVar = 5;
+  print(someVar);
+
   print(country);
-  printHello('You!');
+  printHello('You');
+  printHello(name);
 }
