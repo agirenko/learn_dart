3d4d16a079449e07069f6049756f77f31324451c
agirenko <aspnet123@gmail.com>
2018-07-21 23:09:24 +0300

    Into to classes and objects

--- flutter_dart_udemy_paulo/hello_dart_vars.dart
+++ flutter_dart_udemy_paulo/hello_dart_vars.dart
@@ -1,9 +1,40 @@
+var myOutsideVar = "Outside var";
+// optional positional parameter
+
+String yourAge(String name, String lastName, [int age]) {
+  var result = '$name $lastName';
+  if (age != null) {
+    result = '$name $lastName age: $age';
+  }
+  return result;
+}
+
 main(List<String> arguments) {
   num first = 5.0;
-  num second = 4.1;
+  double second = 4.1;
   var reminder = first % second;
   print(reminder);
 
+  print(num is! String);
+  //print(first as String); error, type 'double' is not a subtype of type 'String' in type cast
+  if (first != 111) {
+    print('first != 11');
+  }
+  for (var i = 0; i < 5; i++) {
+    if (i % 2 == 0) {
+      print('hello $i');
+    }
+  }
+
+  var result = yourAge("Phil", "Collins", 48);
+  print(result);
+  result = yourAge("Cate", "Collins");
+  print(result);
+  print(myOutsideVar);
+
+  insideFunction() {
+    myOutsideVar = "James";
+    print('From inside is: $myOutsideVar');
+  }
+  insideFunction();
 }
-
-
---
+++ flutter_dart_udemy_paulo/intro_to_classes.dart
@@ -0,0 +1,3 @@
+main() {
+  print("Intro to classes");
+}
