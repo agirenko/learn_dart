db4887d350231cf2a17d93d1f783343dfe8ad20a
agirenko <aspnet123@gmail.com>
2018-07-24 22:02:48 +0300

    Dart collections

--- flutter_dart_udemy_paulo/dart_collections.dart
+++ flutter_dart_udemy_paulo/dart_collections.dart
@@ -1,8 +1,33 @@
+class Person {
+  String name;
+
+  Person(this.name);
+
+  @override
+  String toString() {
+    return 'Person {name: $name}';
+  }
+}
+
 main() {
+  //maps
+  var winners = {'first': 'Bill', 'second': 'George', 'third': 'Bond'};
+  print(winners);
+
   List<dynamic> a = [1, 2, 3, "b"];
   var c = {'e': 'b', 'c': 'd', 1: 'f'};
   var list = [10, 2, 13, 24, 100];
   var listAsMap = list.asMap();
+  var listStr = List<String>();
+  var james = Person('James');
+  var sheila = Person('Sheila');
+  var personList = List<Person>();
+  personList.add(james);
+  personList.add(sheila);
+  print(personList);
+  listStr.add('abc');
+  listStr.add('0');
+  print(listStr);
   print(listAsMap);
   print(list.length);
   print(list[0]);
