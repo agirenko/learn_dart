f4edee4e02761967b7c1674d4f89fceb6a689a24
agirenko <aspnet123@gmail.com>
2018-08-17 01:21:32 +0300

    Udemy Dart Bryan Cairns, Intermediary Course

--- flutter_dart_udemy_paulo/final_and_const.dart
+++ flutter_dart_udemy_paulo/final_and_const.dart
@@ -1,10 +1,10 @@
 main() {
-  var country = 'Jamaica';
+
   const pi = 3.14159; // constant at COMPILE time
   print(pi);
   // final - if we want a variable/value to always be constant = never change
   //pi = 33; error
-  final int z = 33;
+
   final listString = ['ab', 'cd'];
   print(listString);
   listString.add('ef');
