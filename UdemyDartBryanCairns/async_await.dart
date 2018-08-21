a458598e2e16ad1f08f05ae52754399e6c0ee119
agirenko <aspnet123@gmail.com>
2018-08-21 15:49:18 +0300

    Udemy Dart Bryan Cairns, Intermediary Course Compression

---
+++ UdemyDartBryanCairns/async_await.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:async';
+
+main(List<String> arguments) async {
+  print('Starting');
+  File file = await appendFile();
+  print('Appended to file ${file.path}');
+  print('*** End');
+}
+
+Future<File> appendFile() async {
+  File file = new File(Directory.current.path + '/test.txt');
+  DateTime now = new DateTime.now();
+  return file.writeAsString(now.toString() + '\r\n', mode: FileMode.append);
+}
--- UdemyDartBryanCairns/async_timers.dart
+++ UdemyDartBryanCairns/async_timers.dart
@@ -3,20 +3,18 @@ import 'dart:async';
 int counter = 0;
 
 main(List<String> arguments) {
-
-  Duration duration = new Duration(seconds: 1);
-  Timer timer = new Timer.periodic(duration, timeout);
+  Duration duration = Duration(seconds: 1);
+  Timer.periodic(duration, timeout);
   print('Started ${getTime()}');
 }
 
 void timeout(Timer timer) {
   print('Timeout: ${getTime()}');
-
   counter++;
-  if(counter >= 5) timer.cancel();
+  if (counter >= 5) timer.cancel();
 }
 
 String getTime() {
-  DateTime dt = new DateTime.now();
+  DateTime dt = DateTime.now();
   return dt.toString();
 }
