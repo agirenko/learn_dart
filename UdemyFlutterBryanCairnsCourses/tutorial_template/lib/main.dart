ee959660f2b5fa52802a9feb026fd6fdc961ed37
agirenko <aspnet123@gmail.com>
2018-08-22 01:28:57 +0300

    flutter beginning course

--- UdemyFlutterBryanCairnsCourses/tutorial_template/lib/main.dart
+++ UdemyFlutterBryanCairnsCourses/tutorial_template/lib/main.dart
@@ -1,37 +1,37 @@
-//import 'package:flutter/material.dart';
-//
-//void main() {
-//  runApp(
-//    MaterialApp(
-//      home: MyApp(),
-//    ),
-//  );
-//}
-//
-//class MyApp extends StatefulWidget {
-//  @override
-//  _State createState() => _State();
-//}
-//
-//class _State extends State<MyApp> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: AppBar(
-//        title: Text(
-//          'Name Here',
-//        ),
-//      ),
-//      body: Container(
-//        padding: EdgeInsets.all(32.0),
-//        child: Center(
-//          child: Column(
-//            children: <Widget>[
-//              Text('Hello World'),
-//            ],
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Name Here',
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
